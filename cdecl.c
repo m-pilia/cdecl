68ea3f19aca0c3dbf6fcd0cd894b275f8f348bb1
m-pilia <martino.pilia@gmail.com>
2015-02-25 18:19:03 +0100

    md fixes

--- cdecl.c
+++ cdecl.c
@@ -28,7 +28,7 @@
  * The declarations are formalized in a simplified way, with a recursive
  * grammar composed by:
  *  - fdecl: a complete declarator (maybe a function, a variable etc.)
- *  - fdecll: a comma separated list of fdcl
+ *  - fdecll: a comma separated list of fdecl
  *  - decl: a declarator with no returning type (maybe a pointer)
  *  - ddecl: a direct declarator, with no returning type and no pointers
  *
@@ -54,9 +54,9 @@
  *
  * Other facts are considered to detect syntax errors:
  *  - a parentheses opened before any identifier must be for gouping
- *  -  + and it cannot be followed by a type name
+ *     + and it cannot be followed by a type name
  *  - a parentheses opened after an identifier must be for function delclaration
- *  -  + and it must be void or followed by a type name
+ *     + and it must be void or followed by a type name
  *  - a square bracket cannot appear if there's no identifier before
  *  - a function cannot return an array nor a function
  */
